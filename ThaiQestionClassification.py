db3d89289129b572be79c1918acec4d7c3b97cf5
Tanakit Sachati <bank21235@Tanakits-MacBook-Pro.local>
2018-11-18 14:42:03 +0700

    bank update

--- ThaiQestionClassification.py
+++ ThaiQestionClassification.py
@@ -2,10 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 from collections import defaultdict
 import pythainlp
+import time
+import codecs
+post = 0;
+Room = "food"
 QUEUE = []
-TITLES = []
 BASEURL = "https://pantip.com/"
-URL = "https://pantip.com/forum?tid=38260667"
+URL = "https://pantip.com/forum/"+Room
 food_count = defaultdict(int)  # default value of int is 0
 
 
@@ -15,6 +18,7 @@ def parse_detail_page(url):
 
 
 def parse_list_page(url):
+    TITLES = []
     data = requests.get(url)
     soup = BeautifulSoup(data.content,'html.parser')
 
@@ -27,12 +31,22 @@ def parse_list_page(url):
     titles = soup.find("div",{"class":"post-list-wrapper"})
     titles2 = titles.find_all("div",{"class" : "post-item"})
     for title in titles2:
+        global post;
+
         qestion = title.find("span", {"class": "icon-mini-posttype-que"})
         if(qestion is not None):
+            post += 1
+            print("processing "+post)
             findalTitle = title.find("div",{"class":"post-item-title"})
+            tags =  title.find_all("div",{"class":"tag-item "})
             # print(findalTitle.text.strip())
-            countPOS(findalTitle.text.strip())
-            TITLES.append(findalTitle.text.strip())
+            taglist = ""
+            for tag in tags:
+                taglist += tag.text.strip()+","
+            taglist = taglist[:-1]
+            row = [findalTitle.text.strip(),taglist]
+            TITLES.append(row)
+    return TITLES
 
 def countPOS(text):
     words = pythainlp.tokenize.word_tokenize(text, engine='mm')
@@ -52,13 +66,23 @@ def main():
     i = 0;
 
     while len(QUEUE):
-        i = i+1
-        if i== 10:
-            break;
+        if post>10000 : break
+        time.sleep(1)
         call_back, url = QUEUE.pop(0)
-        call_back(url)
+        write(call_back(url))
 
     print(food_count)
+
+def write(texts):
+    global Room
+    f = codecs.open(Room+"txt", "a", "utf-8")
+    for i in range(len(texts)):
+
+        f.write(texts[i][0]+"\t"+texts[i][1])
+        f.write("\n")
+
+    f.close()
+
 
 if __name__ == '__main__':
     main()
